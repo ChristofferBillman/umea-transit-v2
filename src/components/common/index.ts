da12eff7f09a7a6896fae148b20f40added7f4eb
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-19 23:54:10 +0100

    feat: all common components can be imported directly from "common"

---
+++ src/components/common/index.ts
@@ -0,0 +1,8 @@
+import Button from './Button'
+import H1 from './H1'
+import Icon from './Icon'
+import NavButton from './NavButton'
+import Card from './Card'
+import Input from './Input'
+
+export { Button, H1, Icon, NavButton, Card, Input }
