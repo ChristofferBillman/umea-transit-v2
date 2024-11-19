dc2467a856681dcc6ac9f969d0923e8fb2c2b92a
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-19 23:57:18 +0100

    feat: App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
-import ExampleComponent from './components/common/ExampleComponent'
+import { ReactNode, useState } from 'react'
+
+import Map from '@components/Map'
+
+import Navbar from '@compositions/Navbar'
+import Menu from '@compositions/Menu'
+import Home from '@compositions/Home'
+import Directions from '@compositions/Directions'
+import Pins from '@compositions/Pins'
 
 import './styles/App.css'
 
-export default function App() {
-	return <ExampleComponent/>
+export const routes = [
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/stops',
+		element: <div>Test</div>,
+	},
+	{
+		path: '/directions',
+		element: <Directions />,
+	},
+	{
+		path: '/settings',
+		element: <div>Test</div>,
+	}
+]
+
+interface Props {
+	outlet: ReactNode
+}
+
+export default function App({ outlet }: Props) {
+
+	const [open, setOpen] = useState(true)
+	const [pinsOpen, setPinsOpen] = useState(false)
+
+	return (
+		<>
+			<Navbar setMenuOpen={setOpen} setPinsOpen={setPinsOpen} />
+			<Map />
+
+			<Menu open={open}>
+				{outlet}
+			</Menu>
+
+			<Pins open={pinsOpen} />
+		</>
+	)
 }
