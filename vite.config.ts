5a11be031c8111c78e6101734b719b491b99c21b
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-19 23:50:26 +0100

    feat: configured import aliases.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import path from 'path'
+
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		react()
 	],
+	resolve: {
+		alias: {
+			'@components': path.resolve(__dirname, './src/components'),
+			'@compositions': path.resolve(__dirname, './src/compositions'),
+			'@hooks': path.resolve(__dirname, './src/hooks'),
+		}
+	}
 })
