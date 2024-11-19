40044e2790af518b014574f158abd27bb3991ae8
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-19 23:51:12 +0100

    feat: deleted components I will not be using.

--- src/components/common/ExampleComponent/ExampleComponent.tsx
+++
@@ -1,9 +0,0 @@
-import css from './ExampleComponent.module.css'
-
-export function ExampleComponent() {
-	return (
-		<div className={css.container}>
-			<p className={css.text}> ⚛️React + 🟦TypeScript + ⚡Vite + 🎨CSS modules </p>
-		</div>
-	)
-}
