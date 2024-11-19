61419213c63555993dbbcd0c9c066db02467b6e9
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-19 23:55:12 +0100

    feat: Directions composition.

---
+++ src/compositions/Directions/Directions.tsx
@@ -0,0 +1,28 @@
+import { Button, H1, Icon } from '@components/common'
+import { Input } from '@components/common'
+
+export function Directions() {
+	return (
+		<>
+			<H1>Hitta resa</H1>
+			<Input
+				placeholder='Från'
+			/>
+			<Icon name='swap_vert' />
+			<Input
+				placeholder='Till'
+			/>
+
+			<div style={{ display: 'flex', marginTop: '1rem', gap: '1rem' }}>
+				<Button>
+					<Icon name='schedule' />
+					Nu
+				</Button>
+				<Button>
+					<Icon name='filter_list' />
+					Filter
+				</Button>
+			</div>
+		</>
+	)
+}
