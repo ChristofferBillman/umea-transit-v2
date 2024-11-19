177bef95cab845304c75730da5ad996e781004cb
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-19 23:52:44 +0100

    feat: added prop size for manually setting its size.

--- src/components/common/Icon/Icon.tsx
+++ src/components/common/Icon/Icon.tsx
@@ -5,14 +5,17 @@ import style from './Icon.module.css'
 interface Props {
 	name: string
 	className?: string
+	size?: string
 }
 
-export function Icon({name, className}: Props) {
+export function Icon({ name, className, size }: Props) {
+
 	return (
 		<span
-            className={clsx('material-symbols-rounded', style.icon, className)}
-        >
-            {name}
-        </span>
+			className={clsx('material-symbols-rounded', style.icon, className)}
+			style={{ fontSize: size }}
+		>
+			{name}
+		</span>
 	)
 }
