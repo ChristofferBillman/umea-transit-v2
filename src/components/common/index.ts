db0df988cb2e55ffcdbdcb3b97b432f97816e2b1
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-21 21:12:23 +0100

    update component exports.

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,5 +4,11 @@ import Icon from './Icon'
 import NavButton from './NavButton'
 import Card from './Card'
 import Input from './Input'
+import Eyebrow from './Eyebrow'
+import P from './P'
+import Error from './Error'
+import E from './E'
+import Skeleton from './Skeleton'
+import M from './M'
 
-export { Button, H1, Icon, NavButton, Card, Input }
+export { Button, H1, Icon, NavButton, Card, Input, Eyebrow, Error, P, E, Skeleton, M }
