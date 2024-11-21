5cf73525d182e4bec5c4e2b8cfbc148bb371baa9
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-21 21:03:45 +0100

    feat: Card now takes prop onClick.

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -8,15 +8,17 @@ interface Props {
 	children: ReactNode
 	as?: 'nav' | 'div'
 	className?: string
+	onClick?: () => void
 }
 
-export function Card({ children, as = 'div', className }: Props) {
+export function Card({ children, as = 'div', className, onClick }: Props) {
 
 	const Wrapper = as
 
 	return (
 		<Wrapper
 			className={clsx(css.card, className)}
+			onClick={onClick}
 		>
 			{children}
 		</Wrapper>
