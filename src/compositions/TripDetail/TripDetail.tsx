2b65df592f7f0257cae6d46a013c0c1d1672aaec
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-24 18:49:38 +0100

    feat: TripDetail is now relatively complete.

--- src/compositions/TripDetail/TripDetail.tsx
+++ src/compositions/TripDetail/TripDetail.tsx
@@ -1,11 +1,17 @@
-import { Button, Icon } from '@components/common'
 import { useLocation, useNavigate } from 'react-router-dom'
+
+import { Button, E, Eyebrow, H1, Icon } from '@components/common'
+import DetailedTripDiagram from '@components/DetailedTripDiagram'
+
+import { Trip } from '@api'
+
+import css from './TripDetail.module.css'
 
 export function TripDetail() {
 
 	const location = useLocation()
 	const navigate = useNavigate()
-	const trip = location.state
+	const trip: Trip = location.state
 
 	if (!trip) return
 
@@ -17,6 +23,16 @@ export function TripDetail() {
 				<Icon name='arrow_back' />
 				Resultat
 			</Button>
+
+			<Eyebrow className={css.eyebrow}>Resedetalj</Eyebrow>
+
+			<H1 className={css.topHeader}>{trip.legs[0].origin.name}</H1>
+			<Icon name='arrow_downward' className={css.headerArrow} />
+			<H1>{trip.legs[trip.legs.length - 1].destination.name}</H1>
+			<E>{trip.duration}</E>
+			<div className={css.scrollContainer}>
+				<DetailedTripDiagram trip={trip} />
+			</div>
 		</>
 	)
 }
